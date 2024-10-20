6c26b994126f49d66f1e96bc880090edf8dd5b66
Daniel Simon <daniel.attila.simon@gmail.com>
2024-10-20 13:24:32 +0700

    chore: remove unused code and deps

--- src/connection.ts
+++ src/connection.ts
@@ -1,14 +1,13 @@
 import type { Provider } from "@ethersproject/abstract-provider";
 import { Networkish, getNetwork } from "@ethersproject/networks";
-import { Batched, WebSocketAugmented } from "@liquity/providers";
+import { Batched } from "@liquity/providers";
 
 import { AlchemyProvider } from "./AlchemyProvider.js";
 
-const BatchedWebSocketAugmentedAlchemyProvider = Batched(WebSocketAugmented(AlchemyProvider));
+const BatchedAlchemyProvider = Batched(AlchemyProvider);
 
 export interface LiquityConnectionOptions {
   alchemyApiKey?: string;
-  useWebSocket?: boolean;
 }
 
 export const getProvider = (
@@ -16,16 +15,9 @@ export const getProvider = (
   options?: LiquityConnectionOptions
 ): Provider => {
   const network = getNetwork(networkish);
-  const provider = new BatchedWebSocketAugmentedAlchemyProvider(network, options?.alchemyApiKey);
+  const provider = new BatchedAlchemyProvider(network, options?.alchemyApiKey);
 
   provider.chainId = network.chainId;
-
-  if (options?.useWebSocket) {
-    provider.openWebSocket(
-      provider.connection.url.replace(/^http/i, "ws").replace(".alchemyapi.", ".ws.alchemyapi."),
-      network
-    );
-  }
 
   return provider;
 };
